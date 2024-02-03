e1f9b23118be922317a39c456ddfb36f1df17b71
Ishvit17 <ishvitbhasin@gmail.com>
2024-02-03 20:27:48 +0530

    RemoveDuplicatesFromSortedArray

---
+++ Easy collection/Array/  RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,18 @@
+class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicate(int[] nums){
+        int k = nums.length;
+        if(k==0 || k==1)
+            return k;
+        int result = 1; // To count the numbers of unique numbers in the array.
+        for(int i = 1; i < k ; i++) {
+            /*
+            If condition to remove the duplicate numbers from the array
+            */
+            if(nums[i] != nums[res-1]){
+                nums[res] = nums[i]; 
+                result++;
+            }
+        }
+        return result;
+    }
+}
